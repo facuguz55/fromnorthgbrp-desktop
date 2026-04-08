4c65c8230bbece6fb8caf2d44f2aeb700b57f813
facuguz55 <facuiguzman1@gmail.com>
2026-04-08 00:37:21 -0300

    Initial desktop app with Tauri auto-updater

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      let handle = app.handle().clone();
+      tauri::async_runtime::spawn(async move {
+        if let Ok(updater) = handle.updater() {
+          if let Ok(Some(update)) = updater.check().await {
+            let _ = update.download_and_install(|_, _| {}, || {}).await;
+          }
+        }
+      });
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
